a335d4a57b5b0e27197174858d92fc8486e92f7e
Ahbapx <ahmetmsekin@gmail.com>
2026-02-23 17:49:24 +0300

    fix(types): support local typecheck for next and next-auth

--- src/components/ui/user-menu.tsx
+++ src/components/ui/user-menu.tsx
@@ -39,7 +39,7 @@ export function UserMenu({
   const userInitials =
     user?.name
       ?.split(" ")
-      .map((n) => n[0])
+      .map((n: string) => n[0])
       .join("")
       .toUpperCase()
       .slice(0, 2) || "U";
